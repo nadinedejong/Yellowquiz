9a21b3903e27e80e5ad8426d2b78b8544751372c
Nadine de Jong <nadinedejong@outlook.com>
2016-11-15 15:30:13 +0100

    Stap 2.1. Tafel en Gast classes verder uitgewerkt

--- src/main/java/nl/youngcapital/Gast.java
+++ src/main/java/nl/youngcapital/Gast.java
@@ -1,5 +1,31 @@
 package nl.youngcapital;
 
 public class Gast {
+	private String naam; 
+	private int leeftijd; 
+	private boolean vrouw;
+	
 
+
+	public boolean isVrouw() {
+		return vrouw;
+	}
+	public void setVrouw(boolean vrouw) {
+		this.vrouw = vrouw;
+	}
+	public String getNaam() {
+		return naam;
+	}
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+	public int getLeeftijd() {
+		return leeftijd;
+	}
+	public void setLeeftijd(int leeftijd) {
+		this.leeftijd = leeftijd;
+	};
+	
+	
+	
 }
